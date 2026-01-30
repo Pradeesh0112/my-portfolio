d40c5e1c41d481a376c8f9d21df9a2f0142f0cd5
Pradeesh0112 <freelancingthink@gmail.com>
2026-01-30 15:26:49 +0530

    Update SocialLinks.tsx
    changed the linkedIn URL.

--- src/app/components/SocialLinks.tsx
+++ src/app/components/SocialLinks.tsx
@@ -9,7 +9,7 @@ export const SocialLinks = () => {
       <a href="https://github.com/pradeeshTech" target="_blank" className="hover:text-green"><FaGithub /></a>
       {/* <a href="https://instagram.com" className="hover:text-green"><FaInstagram /></a> */}
       {/* <a href="https://twitter.com" className="hover:text-green"><FaTwitter /></a> */}
-      <a href="https://linkedin.com/pradeesh-a-7b73aa262" target="_blank" className="hover:text-green"><FaLinkedin /></a>
+      <a href="www.linkedin.com/in/pradeesh-a-7b73aa262" target="_blank" className="hover:text-green"><FaLinkedin /></a>
       <div className="w-[1px] h-24 bg-slate mt-6"></div>
     </div>
   );
